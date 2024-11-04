a82af4faf9509968605370f20ee201536f5d7216
ThanaphatSuporn <thanaphatsupornsecondary@gmail.com>
2024-11-04 10:07:27 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -273,6 +273,51 @@ local function QDRRBL_fake_script() -- Main.FunctionButton
 		end)
 	
 		--//fsddfsdf
+		
+        local function XFJK_fake_script() -- Main.SmoothDrag 
+            local script = Instance.new('LocalScript', Main)
+        
+            local Drag = script.Parent
+            local gsCoreGui = game:GetService("CoreGui")
+            local gsTween = game:GetService("TweenService")
+            local UserInputService = game:GetService("UserInputService")
+                local dragging
+                local dragInput
+                local dragStart
+                local startPos
+                local function update(input)
+                    local delta = input.Position - dragStart
+                    local dragTime = 0.04
+                    local SmoothDrag = {}
+                    SmoothDrag.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+                    local dragSmoothFunction = gsTween:Create(Drag, TweenInfo.new(dragTime, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), SmoothDrag)
+                    dragSmoothFunction:Play()
+                end
+                Drag.InputBegan:Connect(function(input)
+                    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+                        dragging = true
+                        dragStart = input.Position
+                        startPos = Drag.Position
+                        input.Changed:Connect(function()
+                            if input.UserInputState == Enum.UserInputState.End then
+                                dragging = false
+                            end
+                        end)
+                    end
+                end)
+                Drag.InputChanged:Connect(function(input)
+                    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+                        dragInput = input
+                    end
+                end)
+                UserInputService.InputChanged:Connect(function(input)
+                    if input == dragInput and dragging and Drag.Size then
+                        update(input)
+                    end
+                end)
+            
+        end
+
 		notif("loaded! by scuba#0001", 5)
 	end)
 end
